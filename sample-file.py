4b165c4e362b1c1ba42b3ddaa03d6e35c11fdb6f
PiyumiSL <140265767+PiyumiSL@users.noreply.github.com>
2024-12-13 10:48:52 +0530

    Update sample-file.py

--- sample-file.py
+++ sample-file.py
@@ -2,3 +2,13 @@ import streamlit as st
 
 st.write('Hello world!')
 st.title('Synergy prediction of potential drug candidates')
+
+st.header('Upload your training data set here')
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    st.write("File uploaded successfully!")
+    # Display the content of the file (optional)
+    import pandas as pd
+    data = pd.read_csv(uploaded_file)
+    st.write(data)
